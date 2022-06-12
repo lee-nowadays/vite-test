04cf1a3a90ce11d85ac34148d7f9df0ea43bb496
lee-nowadays <qwer7983646@gmail.com>
2022-06-13 02:11:43 +0800

    Add files

---
+++ src/components/LeftMenu.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+#left-menu
+  h1 社團介紹
+  table
+    tr(v-for="club in clubs")
+      td {{ club }}
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+
+const clubs = reactive([
+  '吉他社', '熱舞社', '棒球社', '羽球社', '足球社', '童軍社'
+])
+</script>
+
+<!-- <script>
+import { reactive } from 'vue'
+export default {
+  setup () {
+    const clubs = reactive([
+      '吉他社', '熱舞社', '棒球社', '羽球社', '足球社', '童軍社'
+    ])
+    return {
+      clubs
+    }
+  }
+}
+</script> -->
---
+++ src/components/TopImage.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+#top-image
+  img(src="@/assets/logo.png")
+  //- img(src="../assets/logo2.png")
+  //- img(:src="img")
+</template>
+
+<!-- <script>
+import { ref } from 'vue'
+export default {
+  setup () {
+    const img = ref(new URL('../assets/logo2.png', import.meta.url).href)
+    return {
+      img
+    }
+  }
+}
+</script> -->
+
+<script setup>
+import { ref } from 'vue'
+// const img = ref(new URL('../assets/logo2.png', import.meta.url).href)
+</script>
